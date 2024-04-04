ff2615539089798de49ec3d6d7a7f614342ffbd0
SamTianshiMeng <155681192+SamTianshiMeng@users.noreply.github.com>
2024-04-03 21:15:17 -0400

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -5,6 +5,13 @@ function setLightMode() {
     document.getElementById("Resume").style.color = "rgb(0, 0, 0)";
     document.getElementById("Projects").style.color = "rgb(0, 0, 0)";
     document.getElementById("Interests").style.color = "rgb(0, 0, 0)";
+    document.getElementById("Home").style.color = "rgb(0, 0, 0)";
+    document.getElementById("League").style.color = "rgb(0, 0, 0)";
+    document.getElementById("Naruto").style.color = "rgb(0, 0, 0)";
+    document.getElementById("Bleach").style.color = "rgb(0, 0, 0)";
+    document.getElementById("Fairy tail").style.color = "rgb(0, 0, 0)";
+    document.getElementById("SC2").style.color = "rgb(0, 0, 0)";
+    document.getElementById("Olympic").style.color = "rgb(0, 0, 0)";
     document.getElementById("toggleButton").innerHTML = "On";
 }
 
@@ -14,12 +21,18 @@ function setDarkMode() {
     document.getElementById("Resume").style.color = "rgb(255, 255, 255)";
     document.getElementById("Projects").style.color = "rgb(255, 255, 255)";
     document.getElementById("Interests").style.color = "rgb(255, 255, 255)";
+    document.getElementById("Home").style.color = "rgb(255, 255, 255)";
+    document.getElementById("League").style.color = "rgb(255, 255, 255)";
+    document.getElementById("Naruto").style.color = "rgb(255, 255, 255)";
+    document.getElementById("Bleach").style.color = "rgb(255, 255, 255)";
+    document.getElementById("Fairy tail").style.color = "rgb(255, 255, 255)";
+    document.getElementById("SC2").style.color = "rgb(255, 255, 255)";
+    document.getElementById("Olympic").style.color = "rgb(255, 255, 255)";
     document.getElementById("toggleButton").innerHTML = "On";
 }
-
-
 function myFunction() {
     document.getElementById("textsizechange").style.fontSize = "x-large";
+    document.getElementById("Anime and Games").style.fontSize = "x-large";
   }
 function Original() {
     document.getElementById("textsizechange").style.fontSize = "15px";
